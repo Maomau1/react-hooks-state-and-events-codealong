37d21aaa512b4c5aca41841e487626ecf93d7e62
maurice diong <mdiong00@gmail.com>
2023-12-15 00:36:52 -0500

    added additional features

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setOn]= useState(true)
+  console.log(isOn)
+  
+  return <button style= {{background: isOn? "red" :"white"}} onClick={handleClick}>{isOn? "ON" : "OFF"}</button>;
+
+  function handleClick(){
+    setOn((isOn)=> !isOn);
+  }
 }
 
 export default Toggle;
